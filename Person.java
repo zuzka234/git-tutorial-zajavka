d22e370584081892a65862ab225acfc4740791d7
Zuzanna <zuzanna234@gmail.com>
2023-06-25 23:12:55 +0200

    Person.java.updated
    Signed-off-by: Zuzanna <zuzanna234@gmail.com>

---
+++ Person.java
@@ -0,0 +1,10 @@
+public class Person {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("Hello1!");
+        System.out.println("Hello2!");
+        System.out.println("Hello3!");
+        System.out.println("Hello4!");
+        System.out.println("Hello5!");
+    }
+}
