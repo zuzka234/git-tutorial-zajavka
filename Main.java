ac7f77c66cea0c38ca0061ebea387faf93e33fb8
Zuzanna <zuzanna234@gmail.com>
2023-07-05 22:17:59 +0200

    feat: added some comment
    Signed-off-by: Zuzanna <zuzanna234@gmail.com>

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        //Pull request|| Merge request
+        // feature-branch -> origin/feature-branch -> MR -> Gradle build ->
+        // approve ->(origin/feature-branch=> origin/master)-> git checkout master -> git pull
     }
 }
